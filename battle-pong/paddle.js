5070051353992c9f24abef205ad4c8af885507b8
mozilla <luke.pacholski@gmail.com>
2017-10-25 17:44:01 +0100

    nuked some shit

--- battle-pong/paddle.js
+++ battle-pong/paddle.js
@@ -24,26 +24,6 @@ var paddleGamepadActionMapping = {
   }
 };
 
-
-function connectPaddlesToControls(){
-
-
-  // Let the first paddle use the keyboard regardless
-  paddles[0].addInputComponent(createKeyboardInputComponent(paddleKeyboardActionMapping));
-
-  // For debuggin, it's useful to link up some more
-  // paddles[1].addInputComponent(createKeyboardInputComponent(paddleKeyboardActionMapping));
-  // paddles[2].addInputComponent(createKeyboardInputComponent(paddleKeyboardActionMapping));
-  // paddles[3].addInputComponent(createKeyboardInputComponent(paddleKeyboardActionMapping));
-
-
-  // If there are more than 0 and less than 2 gamepads, hook them up to paddles!
-  for (var i = 0; i < gamepads.length && i < 2; ++i) {
-    connectGamepad(gamepads[i]);
-  }
-}
-
-
 function createPaddle(options) {
   var maxForce = 0.004;
 
