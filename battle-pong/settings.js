6e1f6f46cca68513394300fce6ef1be2e53aeea9
secretrobotron <secretrobotron@gmail.com>
2017-12-01 14:15:53 -0500

    can manipulate settings through query string

--- battle-pong/settings.js
+++ battle-pong/settings.js
@@ -12,3 +12,11 @@ window.Settings = {
   mineForceRadius: 175,
   mineForceMultiplier: 0.00075
 };
+
+var queryStringParts = window.location.search.substr(1).split('&');
+queryStringParts.forEach(function (pair) {
+  var pairParts = pair.split('=');
+  var key = pairParts[0];
+  var value = pairParts[1];
+  if (key in window.Settings) window.Settings[key] = value;
+});
