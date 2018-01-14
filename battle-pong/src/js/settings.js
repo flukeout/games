c65dc694d9ac5f2745ff3d11eed8f92c961b3023
secretrobotron <secretrobotron@gmail.com>
2018-01-14 12:17:43 -0500

    reverted settings

--- battle-pong/src/js/settings.js
+++ battle-pong/src/js/settings.js
@@ -2,11 +2,11 @@ window.Settings = {
   clearSavedControlSettings : true,
   showIntro : false,
   debug : true,
-  // powerUpTypes : ["mine", "clone", "grow", "spin"], //"multiball"
-  powerUpTypes : ["mine"],
+  powerUpTypes : ["mine", "clone", "grow", "spin"], //"multiball"
+  // powerUpTypes : ["mine"],
   brakesModeEnabled : false,
   goalTimeoutMS : 250,
-  powerupFrequency : 30, //300
+  powerupFrequency : 300, //300
   maxPowerupCount : 2, // 2
   playTo : 2,
   mineForceRadius: 175,
