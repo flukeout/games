0c5996c4fda6331b458404249b6e4ecfdb984034
mozilla <luke.pacholski@gmail.com>
2018-03-29 22:50:21 -0700

    Updated gamepad controls

--- battle-pong/src/js/menu-controls.js
+++ battle-pong/src/js/menu-controls.js
@@ -2,19 +2,6 @@ let buttons;
 let selectedButton;
 let selectedIndex;
 
-const selectButton = num => {
-  selectedIndex = num || 0;
-  selectedButton = buttons[num];
-  selectedButton.classList.add('input-selected');
-}
-
-const deselectAllButtons = () => {
-  buttons.forEach(button => {
-    button.classList.remove('input-selected');
-  });
-  selectedButton = false;
-}
-
 const setupInputButtons = defaultButtonNum => {
   
   buttons = Array.prototype.slice.call(document.querySelectorAll('.gamepad-button'));
@@ -58,25 +45,44 @@ const setupInputButtons = defaultButtonNum => {
 
 function moveCursor(direction) {
 
+  
+
   if(!selectedButton) {
     return;
   }
 
-  if (direction === 'right') {
-    selectedIndex++;
-    if(selectedIndex >= buttons.length) {
-      selectedIndex = 0;
-    }
-  }
+ 
 
-  if (direction === 'left') {
-    selectedIndex--;
-    if(selectedIndex < 0) {
-      selectedIndex = buttons.length - 1;
-    }
-  }
+  // if (direction === 'right') {
+  //   selectedIndex++;
+  //   if(selectedIndex >= buttons.length) {
+  //     selectedIndex = 0;
+  //   }
+  //   deselectAllButtons();
+  // }
 
-  deselectAllButtons();
+  findButton(selectedButton, direction);
+//   if (direction === 'down') {
+//     findButton(selectedButton, 'down');
+//   }
+
+//   if (direction === 'up') {
+//     findButton(selectedButton, 'up');
+//   }
+
+// if (direction === 'left') {
+//     findButton(selectedButton, 'left');
+//   }
+
+  // if (direction === 'left') {
+  //   selectedIndex--;
+  //   if(selectedIndex < 0) {
+  //     selectedIndex = buttons.length - 1;
+  //   }
+  //   deselectAllButtons();
+  // }
+
+  
 
   selectedButton = buttons[selectedIndex];
   selectedButton.classList.add('input-selected');
@@ -87,4 +93,105 @@ function moveCursor(direction) {
       addTemporaryClassName(selectedButton, "poke", 250);
     }
   }
+}
+
+const getCenter = el => {
+  let rect = el.getBoundingClientRect();
+  return {
+    x : Math.floor(rect.left + rect.width / 2),
+    y : Math.floor(rect.top + rect.height / 2)
+  }
+}
+
+const getOptions = (baseCenter, buttons, direction) => {
+  let options = [];
+
+  buttons.forEach(button => {
+    let center = getCenter(button);
+    let conditionMet = false;
+
+    if(direction === "down" 
+        && center.y > baseCenter.y
+        && Math.abs(center.x - baseCenter.x) < 75) {
+      conditionMet = true;
+    }
+
+    if(direction === "up" 
+        && center.y < baseCenter.y
+        && Math.abs(center.x - baseCenter.x) < 75) {
+      conditionMet = true;
+    }
+
+    if(direction === "left" 
+        && center.x < baseCenter.x
+        && Math.abs(center.y - baseCenter.y) < 75) {
+      conditionMet = true;
+    }
+
+    if(direction === "right" 
+        && center.x > baseCenter.x
+        && Math.abs(center.y - baseCenter.y) < 75) {
+      conditionMet = true;
+    }
+    
+    if(conditionMet) {
+      options.push({
+        el: button,
+        delta: Math.sqrt(Math.pow((center.x - baseCenter.x), 2) + Math.pow((center.y - baseCenter.y), 2))
+      });
+    }
+  });
+
+  return options;
+}
+
+let lowestY;
+let highestY;
+
+const findButton = (thisButton, direction) => {
+
+  let thisCenter = getCenter(thisButton);
+  let options = getOptions(thisCenter, buttons, direction);
+
+  if(options.length == 0) {
+    return;
+  }
+
+  let minDistance = false;
+  let closestButton;
+
+  options.forEach(option => {
+    if(!minDistance){
+      minDistance = option.delta;
+      closestButton = option.el;
+    }
+    if(option.delta < minDistance) {
+      minDistance = option.delta;
+      closestButton = option.el;
+    }
+  });
+
+  selectButtonEl(closestButton);
+}
+
+
+const selectButtonEl = el => {
+  deselectAllButtons();
+  selectedIndex = buttons.indexOf(el);
+  selectedButton = el;
+  el.classList.add('input-selected');
+}
+
+const selectButton = num => {
+  selectedIndex = num || 0;
+  selectedButton = buttons[num];
+  selectedButton.classList.add('input-selected');
+}
+
+const deselectAllButtons = () => {
+  
+  buttons.forEach(button => {
+    button.classList.remove('input-selected');
+  });
+  selectedButton = false;
 }
