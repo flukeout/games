a4e0a8820095d254e4e9ca60ea089ccaca4a4ee0
secretrobotron <secretrobotron@gmail.com>
2018-05-12 13:05:17 -0400

    Story in splash screen
    Looks cool.

--- battle-pong/src/js/splash.js
+++ battle-pong/src/js/splash.js
@@ -9,6 +9,68 @@ let bestOfEls,
 
 let timeoutAccumulator = 0;
 
+let story = [
+  '22,018 Galactic Era',
+  'Long since humanity\'s ascent to hyper-cybernetics, Earth\'s descendants have achieved their optimal form.',
+  'Scattered throughout the galaxy, rival factions of the once proud and unified Photospheric Orbital Nova Group compete for resources.',
+  'The two most powerful, Purpolium and Greebesque have negotiating for control of supermassive black hole at the galactic center.',
+  'Inevitably conflict arises, and the two factions agree to settle the confrontation through modern diplomatic means.',
+  'As they summon their most elite warriors from the corners of the Milky Way, the entire galaxy awaits in anticipation of organized warfare on a scale never before seen...'
+];
+
+function startStory(finishedCallback) {
+  let storyScreen = document.querySelector('#story');
+  let typer = storyScreen.querySelector('.typer');
+  let skipButton = storyScreen.querySelector('.skip');
+  let typingInterval;
+  let typingNextTimeout;
+
+  function finished() {
+    storyScreen.classList.remove('show');
+    oldStarsParent.appendChild(stars);
+    finishedCallback();
+  }
+
+  let stars = document.querySelector('.canvas-stars');
+  let oldStarsParent = stars.parentNode;
+  document.body.appendChild(stars);
+
+  storyScreen.classList.add('show');
+
+  function typeNext() {
+    typer.textContent = '';
+
+    let currentLine = story.shift();
+
+    if (currentLine) {
+      let index = 0;
+      typingInterval = setInterval(() => {
+        let letter = currentLine[index++];
+
+        if (letter) {
+          SoundManager.playSound('Menu_Move', {volume: .25});
+          typer.textContent += letter;
+        }
+        else {
+          clearInterval(typingInterval);
+          typingNextTimeout = setTimeout(typeNext, 2000);
+        }
+      }, 25);
+    }
+    else {
+      finished();
+    }
+  }
+
+  typeNext();
+
+  skipButton.addEventListener('click', (e) => {
+    clearTimeout(typingNextTimeout);
+    clearInterval(typingInterval);
+    finished();
+  });
+}
+
 window.addEventListener('load', function(){
   let readyScreen = document.querySelector('#ready');
 
@@ -18,11 +80,13 @@ window.addEventListener('load', function(){
 
     readyScreen.querySelector('.ok').addEventListener('click', () => {
       readyScreen.classList.remove('show');
-      document.querySelector(".splash").classList.add("appear");
-      SoundManager.musicEngine.cueSong('menu');
-      SoundManager.resumeAudioContext();
-      if (Settings.music) SoundManager.musicEngine.fadeIn( 2, {loop: true} );
-      startCredits(CREDITS_DELAY);
+      startStory(() => {
+        document.querySelector(".splash").classList.add("appear");
+        SoundManager.musicEngine.cueSong('menu');
+        SoundManager.resumeAudioContext();
+        if (Settings.music) SoundManager.musicEngine.fadeIn( 2, {loop: true} );
+        startCredits(CREDITS_DELAY);        
+      });
     });
 
     setupInputButtons();
