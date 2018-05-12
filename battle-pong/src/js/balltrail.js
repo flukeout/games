c3ebe53486c84422b43b6a6371ebe6fc9d1b0a84
mozilla <luke.pacholski@gmail.com>
2018-05-12 16:30:48 -0700

    Ball trail updates

--- battle-pong/src/js/balltrail.js
+++ battle-pong/src/js/balltrail.js
@@ -23,34 +23,48 @@ let ballPosition = [];
 let lastPos = {};
 let ticks = 0;
 let maxTrailLength = 20;
+let maxDisplayRatio = 1;
 let frames = 0;
 let lastColor = false;
+let cutoffSpeed = 4;
+let displaySteps;
+let desiredDisplaySteps;
+let actualDisplaySteps = 0;
 
 function drawTrail() {
     frames++;
-
+    let ball = game.balls[0];
+    let touchedAnything = false;
     let pushing = false;
+    
+    if(ball) {
+      touchedAnything = ball.lastTouchedPaddle === 1 || ball.lastTouchedPaddle === 2 ? true : false;
+      maxDisplayRatio = mapScale(ball.physics.speed, 2, 10, 0, 1);
+    }
 
-    if(game.balls[0]) {
-      if(game.balls[0].hasTrail) {
+    if(ball && touchedAnything) {
+
+      if(ball.lastTouchedPaddle === 1) {
+        lastColor = "#c15db5";
+      }
+      
+      if(ball.lastTouchedPaddle === 2) {
+        lastColor = "#4dc18f";
+      }
+
+      if(ball.isSpinning) {
+        lastColor = "#39f6ff"
+      }
+
+      if(ball.hasTrail) {
         pushing = true;
         ballPosition.push({
           x : parseFloat(game.balls[0].physics.position.x),
           y : parseFloat(game.balls[0].physics.position.y),
           speed : parseFloat(game.balls[0].physics.speed),
-          frame : parseInt(frames)
+          frame : parseInt(frames),
+          color : lastColor
         });
-      }
-      if(game.balls[0].lastTouchedPaddle === 1) {
-        lastColor = "#c15db5";
-      }
-      
-      if(game.balls[0].lastTouchedPaddle === 2) {
-        lastColor = "#4dc18f";
-      }
-
-      if(game.balls[0].isSpinning) {
-        lastColor = "#39f6ff"
       }
     }
 
@@ -58,34 +72,68 @@ function drawTrail() {
       ballPosition.shift();
     }
 
+
     if(pushing == false && ballPosition.length > 0) {
-     ballPosition.shift(); 
+      ballPosition.shift(); 
     }
     
     trailCtx.clearRect(0, 0, trailCanvasWidth, trailCanvasHeigth); // Clear canvas
 
-    for(var i = 1; i < ballPosition.length; i++) {
+    let displaySteps = parseInt(ballPosition.length * maxDisplayRatio);
+
+    let maxDisplaySteps = parseInt(maxDisplayRatio * ballPosition.length);
+    let j = 1;
+
+
+    if(ball) {
+      if(ball.isSpinning) {
+        maxDisplaySteps = 20;
+      }
+    }
+
+
+    if(actualDisplaySteps < maxDisplaySteps) {
+      actualDisplaySteps++;
+    } else if (actualDisplaySteps > maxDisplaySteps) {
+      actualDisplaySteps--;
+    }
+
+
+    for(var i = (ballPosition.length - actualDisplaySteps) + 1; i < ballPosition.length; i++) {
+
+      // Gets drawn from end tip to ball.
 
       let pos = ballPosition[i];
       let prevPos = ballPosition[i-1];
 
       trailCtx.lineJoin = trailCtx.lineCap = 'round';
       trailCtx.beginPath();
-      trailCtx.strokeStyle = lastColor;
+      trailCtx.strokeStyle = pos.color;
       trailCtx.moveTo(prevPos.x, prevPos.y);
-      let sizeMult = 1;
+      
+      // let progress = i / (ballPosition.length - 1);
+      let progress = j / actualDisplaySteps;
+      j++;
+      let sizeMult = EasingFunctions.easeOutQuart(progress);
 
-      if(i < (ballPosition.length / 2)) {
-        sizeMult = EasingFunctions.easeOutQuad((ballPosition.length - i)/ (ballPosition.length/2));
-      }
-      
       trailCtx.lineTo(pos.x, pos.y);
-      trailCtx.lineWidth = 30 * sizeMult;
-      
+      trailCtx.lineWidth = 36 * sizeMult;
+
       if(lastColor) {
         trailCtx.stroke();
       }
-      
+    }
+
+    // Glowy outline duder
+    if(ball && touchedAnything){
+      if(ball.physics.speed < cutoffSpeed) {
+        let sinMult = Math.sin(frames/6);
+        let radius = 30 + (sinMult * 4);
+        trailCtx.beginPath();
+        trailCtx.arc(ball.physics.position.x, ball.physics.position.y, radius, 0, 2 * Math.PI, false);
+        trailCtx.fillStyle = lastColor;
+        trailCtx.fill();
+      }
     }
 }
 
